760170f19a423cf233270925f11c36f5a97a8f74
Kain <khanhhung.nguyen.dev@gmail.com>
2024-07-21 17:11:19 +0900

    simple router

--- main.go
+++ main.go
@@ -5,22 +5,57 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
-func messageHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to Go Web Development")
+type Node struct {
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedOn   time.Time `json:"createdon"`
+}
+
+var nodes = make(map[string]Node)
+
+var id int = 0
+
+// POST node - /api/nodes
+func postNodeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "POST /nodes")
+}
+
+// GET node - /api/nodes
+func getNodeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "GET /nodes")
+}
+
+// PUT node - /api/nodes/{id}
+func putNodeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "PUT /nodes/{id}")
+}
+
+// DELETE node - /api/nodes/{id}
+func deleteNodeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "DELETE /nodes/{id}")
 }
 
 func main() {
-	http.HandleFunc("/message", messageHandler)
+	router := mux.NewRouter()
 
-	server := http.Server{
+	router.HandleFunc("/api/nodes", getNodeHandler).Methods("GET")
+	router.HandleFunc("/api/nodes", postNodeHandler).Methods("POST")
+	router.HandleFunc("/api/nodes/{id}", putNodeHandler).Methods("PUT")
+	router.HandleFunc("/api/nodes/{id}", deleteNodeHandler).Methods("DELETE")
+
+	server := &http.Server{
 		Addr:           ":8080",
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
+		Handler:        router,
 	}
 
-	log.Println("Listening...")
+	log.Println("Listening on port 8080...")
+
 	server.ListenAndServe()
 }
